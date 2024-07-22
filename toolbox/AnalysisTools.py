fff0efde9648f0d03db0261a6c541250a69e71da
XimeCraft <santochaoya@yahoo.com.hk>
2024-07-22 20:27:47 +0800

    add spaceship

---
+++ toolbox/AnalysisTools.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+class AnalysisToolbox():
+
+    def __init__(self) -> None:
+        pass
+        
+    def descripbe_num_features(self, df):
+        
+        df_num = df.select_dtypes(include=[int, float])
+
+        df_num.describe().T\
+            .join(pd.DataFrame(df_num.skew(), columns=['Skew']))\
+            .join(pd.DataFrame(df_num.kurtosis(), columns=['Kurtosis']))\
+            .join(pd.DataFrame(df_num.std()/df_num.mean(), columns=['Coefficient of Variation']))
+        
+    
